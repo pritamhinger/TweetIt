f5849f86a0fc1910851a439fee47eeaa20c5a362
pritam hinger <pritamgenius24@gmail.com>
2017-12-13 00:03:05 +0530

    Adding UI Components and adding contraints.

--- TweetIt/View/CustomCells.swift
+++ TweetIt/View/CustomCells.swift
@@ -50,21 +50,57 @@ class HomeCell: DatasourceCell {
     
     override var datasourceItem: Any?{
         didSet{
-            titleLabel.text = datasourceItem as? String
+            nameLabel.text = datasourceItem as? String
         }
     }
     
-    let titleLabel: UILabel = {
+    let profileImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.layer.cornerRadius = 25.0
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "Test Test Test"
+        label.backgroundColor = .green
         return label
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "username"
+        label.backgroundColor = .purple
+        return label
+    }()
+    
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .yellow
+        return textView
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        return button
     }()
     
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .yellow
         
-        addSubview(titleLabel)
-        titleLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        addSubview(profileImageView)
+        addSubview(nameLabel)
+        addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
+        
+        profileImageView.anchor(topAnchor, left: leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 8, rightConstant: 12, widthConstant: 0, heightConstant: 0)
+        followButton.anchor(profileImageView.topAnchor, left: nil, bottom: nil, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 8, rightConstant: 12, widthConstant: 120, heightConstant: 34)
     }
 }
