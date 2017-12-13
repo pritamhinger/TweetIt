a5175111f1a6ec66e0dc9bcc4581cecda5b34a26
pritam hinger <pritamgenius24@gmail.com>
2017-12-13 23:50:29 +0530

    Implementated the mockup design.

--- TweetIt/View/CustomCells.swift
+++ TweetIt/View/CustomCells.swift
@@ -8,41 +8,40 @@
 
 import LBTAComponents
 
+let color = UIColor(r: 61, g: 167, b: 244)
+
 class HomeHeader: DatasourceCell{
     
     let headerLabel: UILabel = {
         let label = UILabel()
-        label.text = "Header Title"
-        label.font = UIFont.boldSystemFont(ofSize: 14)
-        label.textColor = .white
+        label.text = "WHO TO FOLLOW"
+        label.font = UIFont.systemFont(ofSize: 16)
+        
         return label
     }()
     
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .blue
         
         addSubview(headerLabel)
-        headerLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        headerLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
     }
 }
 
 class HomeFooter: DatasourceCell {
     let footerLabel: UILabel = {
         let label = UILabel()
-        label.text = "Footer Notes"
-        label.textAlignment = .right
+        label.text = "Show me more"
         label.font = UIFont.systemFont(ofSize: 14)
-        label.textColor = UIColor.purple
+        label.textColor = color
         return label
     }()
     
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .green
         
         addSubview(footerLabel)
-        footerLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 8, widthConstant: 0, heightConstant: 0)
+        footerLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 8, widthConstant: 0, heightConstant: 0)
     }
 }
 
@@ -56,35 +55,48 @@ class HomeCell: DatasourceCell {
     
     let profileImageView : UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .red
-        imageView.layer.cornerRadius = 25.0
-        imageView.layer.masksToBounds = true
+        imageView.image = #imageLiteral(resourceName: "profile_picture")
+        imageView.layer.cornerRadius = 5.0
+        imageView.clipsToBounds = true
         return imageView
     }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
-        label.text = "Test Test Test"
-        label.backgroundColor = .green
+        label.text = "Pritam Hinger"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
         return label
     }()
     
     let usernameLabel: UILabel = {
         let label = UILabel()
-        label.text = "username"
-        label.backgroundColor = .purple
+        label.text = "@pritamhinger"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = UIColor(r: 130, g: 130, b: 130, a: 1)
         return label
     }()
     
     let bioTextView: UITextView = {
         let textView = UITextView()
-        textView.backgroundColor = .yellow
+        textView.text = "An ios developer by passion. Contact to get a beautifully hand crafted app for your business house. Write us at artist@appdevelapp.com or tweet us @pritamhinger"
+        textView.font = UIFont.systemFont(ofSize: 15)
+        textView.backgroundColor = .clear
         return textView
     }()
     
     let followButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .cyan
+        
+        button.layer.cornerRadius = 5.0
+        button.layer.borderColor = color.cgColor
+        button.layer.borderWidth = 1
+        button.setTitle("Follow", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(color, for: .normal)
+        button.setImage(#imageLiteral(resourceName: "icons8-twitter"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: -60, bottom: 0, right: 0)
         return button
     }()
     
@@ -99,8 +111,8 @@ class HomeCell: DatasourceCell {
         
         profileImageView.anchor(topAnchor, left: leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
         nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
-        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
-        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 8, rightConstant: 12, widthConstant: 0, heightConstant: 0)
+        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: -4, bottomConstant: 8, rightConstant: 12, widthConstant: 0, heightConstant: 0)
         followButton.anchor(profileImageView.topAnchor, left: nil, bottom: nil, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 8, rightConstant: 12, widthConstant: 120, heightConstant: 34)
     }
 }
