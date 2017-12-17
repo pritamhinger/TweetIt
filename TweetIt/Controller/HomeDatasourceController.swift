84be99021d441f78d42bae0bc7858119ca1302ac
pritam hinger <pritamgenius24@gmail.com>
2017-12-17 15:57:28 +0530

    Setting up controls on NavigationBar.

--- TweetIt/Controller/HomeDatasourceController.swift
+++ TweetIt/Controller/HomeDatasourceController.swift
@@ -12,9 +12,52 @@ import LBTAComponents
 
 class HomeDatasourceController: DatasourceController {
     
+    let frame = CGRect(x: 0, y: 0, width: 32, height: 32)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.datasource = HomeDatasource()
+        setUpNavigationBarItems()
+    }
+    
+    func setUpNavigationBarItems() {
+        setUpLeftNavItems()
+        setUpRightNavItems()
+        setUpRemainingNavItems()
+    }
+    
+    private func setUpLeftNavItems(){
+        let followButton = UIButton(type: .system)
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "icons8-add_administrator"))
+        imageView.contentMode = .scaleAspectFit
+        followButton.setImage(#imageLiteral(resourceName: "icons8-add_administrator").withRenderingMode(.alwaysOriginal), for: .normal)
+        followButton.frame = frame
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
+    }
+    
+    private func setUpRightNavItems(){
+        
+        let searchButton = UIButton(type: .system)
+        searchButton.setImage(#imageLiteral(resourceName: "icons8-search").withRenderingMode(.alwaysOriginal), for: .normal)
+        searchButton.frame = frame
+        let searchBarButton = UIBarButtonItem(customView: searchButton)
+        
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "icons8-composing_mail_filled").withRenderingMode(.alwaysOriginal), for: .normal)
+        composeButton.frame = frame
+        let composeBarButton = UIBarButtonItem(customView: composeButton)
+
+        navigationItem.rightBarButtonItems = [composeBarButton, searchBarButton]
+    }
+    
+    private func setUpRemainingNavItems(){
+        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "icons8-twitter"))
+        titleImageView.contentMode = .scaleAspectFit
+        titleImageView.frame = frame
+        navigationItem.titleView = titleImageView
+        
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.backgroundColor = .white
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
